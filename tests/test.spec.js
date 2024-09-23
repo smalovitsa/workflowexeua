d1738d05a7c6389690db1b759b4211ed31eb5cae
Ubuntu <stas.malovitsa@gmail.com>
2024-09-23 17:38:38 +0300

    more test tries

--- tests/test.spec.js
+++ tests/test.spec.js
@@ -5,4 +5,12 @@ test('check page title', async ({page}) => {
 
     const title = await page.title();
     expect(title).toBe('EXE.ua');
+    
+});
+
+test('check page title 2', async ({page}) => {
+    await page.goto('https://exe.ua');
+
+    const title = await page.title();
+    expect(title).toBe('EXE.ua');
 });
