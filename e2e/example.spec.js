0ef5c77547ebbbbf7407101d5595a4a349b6972f
Ubuntu <stas.malovitsa@gmail.com>
2024-09-23 18:16:07 +0300

    added right tests

--- e2e/example.spec.js
+++ e2e/example.spec.js
@@ -1,19 +1,22 @@
 // @ts-check
 const { test, expect } = require('@playwright/test');
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('check page title', async ({page}) => {
+  await page.goto('https://exe.ua');
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  const title = await page.title();
+  expect(title).toBe("Інтернет-магазин exe.ua: сервери, серверне, промислове обладнання, комп'ютери, комплектуючі, ноутбуки, послуги | Опт, роздріб, ПДВ, ТОВ");
+  
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+// test('header of exe.ua', async ({ page }) => {
+//   await page.goto('https://exe.ua');
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+//   const header = await page.locator('header');
+  
+  
+//   await expect(header).toBeVisible();
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
+//   const logo = await page.locator('.logo');
+//   await expect(logo).toBeVisible();
+// });
--- tests/test.spec.js
+++ tests/test.spec.js
@@ -8,9 +8,15 @@ test('check page title', async ({page}) => {
     
 });
 
-test('check page title 2', async ({page}) => {
-    await page.goto('https://exe.ua');
 
-    const title = await page.title();
-    expect(title).toBe('EXE.ua');
+test('header of exe.ua', async ({ page }) => {
+  await page.goto('https://exe.ua');
+
+  const header = await page.locator('header');
+  
+  
+  await expect(header).toBeVisible();
+
+  const logo = await page.locator('.logo');
+  await expect(logo).toBeVisible();
 });
