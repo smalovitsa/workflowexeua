9f13933bad49e0e94decbba7caea070ecb4659b0
Ubuntu <stas.malovitsa@gmail.com>
2024-09-25 22:00:35 +0300

    format done

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,9 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
 export default [
-  {ignores : ["e2e/", "playwright.config.js", "tests/"]},
-  {files: ["**/*.js"], languageOptions: {sourceType: "script"}},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
+	{ ignores: ["e2e/", "playwright.config.js", "tests/"] },
+	{ files: ["**/*.js"], languageOptions: { sourceType: "script" } },
+	{ languageOptions: { globals: globals.browser } },
+	pluginJs.configs.recommended,
 ];
