d4701106d7a2f92320fa6d9fbbb0e8a602ad2db1
Ubuntu <stas.malovitsa@gmail.com>
2024-10-04 15:21:31 +0300

    fix(eslint-report): fixed dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,7 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 
 export default [
-	{ ignores: ["e2e/", "playwright.config.js", "tests/"] },
+	{ ignores: ["e2e/", "playwright.config.js", "tests/", "npm-audit-report"] },
 	{ files: ["**/*.js"], languageOptions: { sourceType: "script" } },
 	{ languageOptions: { globals: globals.browser } },
 	pluginJs.configs.recommended,
